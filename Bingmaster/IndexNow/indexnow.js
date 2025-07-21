13853c9b322356e5840323c8187edf67f43a8a60
Hayatul1 <makky300687@gmail.com>
2025-07-21 17:14:51 +0700

    Delete Bingmaster/IndexNow/indexnow.js

--- Bingmaster/IndexNow/indexnow.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener("DOMContentLoaded", function () {
-  const indexNowURL = "https://www.bing.com/indexnow?url=" + encodeURIComponent(window.location.href) + "&key=2c551ea6ba90422cb715a280c6023eb7";
-  fetch(indexNowURL)
-    .then(response => console.log("IndexNow success:", response.status))
-    .catch(error => console.warn("IndexNow failed:", error));
-});
