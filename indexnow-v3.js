0e3070d82a4ffc1b64d740dd03e125fbdd8942af
Hayatul1 <makky300687@gmail.com>
2025-07-21 21:25:12 +0700

    Add files via upload

---
+++ indexnow-v3.js
@@ -0,0 +1,24 @@
+
+(function () {
+  try {
+    var bingEndpoint = "https://www.bing.com/indexnow";
+    var key = "a1527aefef5c4d62bec596081250ff9c";
+    var currentURL = location.href;
+
+    var pingURL = bingEndpoint + "?url=" + encodeURIComponent(currentURL) + "&key=" + key;
+
+    fetch(pingURL)
+      .then(function (response) {
+        if (!response.ok) {
+          console.warn("IndexNow ping failed:", response.status);
+        } else {
+          console.log("IndexNow ping success:", response.status);
+        }
+      })
+      .catch(function (error) {
+        console.error("IndexNow request error:", error);
+      });
+  } catch (e) {
+    console.error("IndexNow script error:", e);
+  }
+})();
