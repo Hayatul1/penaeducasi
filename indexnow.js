431fbe61bb2b7d98979bd7ff2b411481279bef69
Hayatul1 <makky300687@gmail.com>
2025-07-21 19:03:48 +0700

    Delete indexnow.js

--- indexnow.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener("DOMContentLoaded", function () {
-  const indexNowURL = "https://www.bing.com/indexnow?url=" + encodeURIComponent(window.location.href) + "&key=a1527aefef5c4d62bec596081250ff9c";
-  fetch(indexNowURL)
-    .then(response => console.log("IndexNow success:", response.status))
-    .catch(error => console.warn("IndexNow failed:", error));
-});
